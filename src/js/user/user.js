c037460723cfaf3c3421b264f61e477298b9bea7
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2023-04-28 18:07:35 +0100

    gltf2 done

--- src/js/user/user.js
+++ src/js/user/user.js
@@ -14,7 +14,7 @@ export class UserControls {
     static model = new THREE.Group();
     static scene = new THREE.Scene();
 
-    constructor(model,scene) {
+    constructor(model, scene) {
         this.model = model;
         this.scene = scene;
     }
@@ -51,99 +51,116 @@ export class UserControls {
                 this.placeObject(this.model, this.scene);
                 break;
         }
-    }
-
-    modelHasObjectInFront() {
-        // Create a raycaster from the model's position in the direction they are facing
-        const raycaster = new THREE.Raycaster();
-        const direction = new THREE.Vector3();
-        const modelPosition = this.model.position.clone();
-        modelPosition.y += 2; // Move the raycaster up to the model's height
-        direction.set(0, 0, -1); // Set the direction based on the model's rotation
-        direction.applyQuaternion(this.model.quaternion);
-        raycaster.set(modelPosition, direction);
-
-        // Check for intersections with objects in the scene
-        const intersects = raycaster.intersectObjects(this.scene.children, true);
-        
-        // Loop through the intersections and check the distance
-        const closestIntersection = intersects.find(intersect => intersect.object.name === "target");
-        if (closestIntersection) {
-            return closestIntersection.object;
+        // Make sure the user stays within the x-coordinate bounds
+        if (this.model.position.x < 0) {
+            this.model.position.setX(0);
+        } else if (this.model.position.x > 50) {
+            this.model.position.setX(50);
         }
 
-        return false;
-    }
-
-    grabObject() {
-        // Check if there is an object in front of the model
-        let object = this.modelHasObjectInFront();
-        if (object == false) {
-            // Perform grabbing logic here
-            console.log("No object in front to grab.");
-            return;
+        if (this.model.position.z < 0) {
+            this.model.position.setZ(0);
+        } else if (this.model.position.z > 50) {
+            this.model.position.setZ(50);
         }
 
-        if (object.name != "target") {
-            console.log("Can't grab this object.");
-            return;
-        }
-        inventory.push(object);
-        this.scene.remove(object);
-
-        this.sendInventory();
+        spotlight.target.position.set(this.model.position.x + Math.sin(angle), 5, this.model.position.z + Math.cos(angle));
+        spotlight.target.updateMatrixWorld();
+        spotlight.updateMatrixWorld();
     }
 
-    sendInventory() {
+
+modelHasObjectInFront() {
+    // Create a raycaster from the model's position in the direction they are facing
+    const raycaster = new THREE.Raycaster();
+    const direction = new THREE.Vector3();
+    const modelPosition = this.model.position.clone();
+    modelPosition.y += 2; // Move the raycaster up to the model's height
+    direction.set(0, 0, -1); // Set the direction based on the model's rotation
+    direction.applyQuaternion(this.model.quaternion);
+    raycaster.set(modelPosition, direction);
+
+    // Check for intersections with objects in the scene
+    const intersects = raycaster.intersectObjects(this.scene.children, true);
+
+    // Loop through the intersections and check the distance
+    const closestIntersection = intersects.find(intersect => intersect.object.name === "target");
+    if (closestIntersection) {
+        return closestIntersection.object;
+    }
+
+    return false;
+}
+
+grabObject() {
+    // Check if there is an object in front of the model
+    let object = this.modelHasObjectInFront();
+    if (object == false) {
+        // Perform grabbing logic here
+        console.log("No object in front to grab.");
+        return;
+    }
+
+    if (object.name != "target") {
+        console.log("Can't grab this object.");
+        return;
+    }
+    inventory.push(object);
+    this.scene.remove(object);
+
+    this.sendInventory();
+}
+
+sendInventory() {
+    const coin = document.createElement('img');
+    coin.src = './img/coin.png';
+    try {
+        while (inventory_container.firstChild) {
+            inventory_container.removeChild(inventory_container.firstChild);
+        }
+    } catch (e) {
+        console.log(e);
+    }
+
+    for (let i = 0; i < inventory.length; i++) {
         const coin = document.createElement('img');
         coin.src = './img/coin.png';
+        inventory_container.appendChild(coin);
+    }
+}
+
+getInventory() {
+    return inventory;
+}
+
+placeObject() {
+    // Check if there is an object in front of the model
+    if (this.modelHasObjectInFront() !== false) {
+        console.log("Has an object in front.");
+        return;
+    }
+    // Check if the model has an object in their inventory
+    if (inventory.length == 0) {
+        console.log("No object in inventory to place.");
+        return;
+    } else {
+        let object = inventory.pop();
+        // Set the position of the object in front of the model
+        const modelDirection = new THREE.Vector3();
+        this.model.getWorldDirection(modelDirection);
+        const objectPosition = this.model.position.clone().addScaledVector(modelDirection, -4); // Place the object 4 units in front of the model
+        object.position.copy(objectPosition);
+        object.position.y = 2;
+        this.scene.add(object);
+        console.log("Object Placed.")
         try {
-            while (inventory_container.firstChild) {
-                inventory_container.removeChild(inventory_container.firstChild);
-            }
+            inventory_container.removeChild(inventory_container.childNodes[0]);
         } catch (e) {
-            console.log(e);
-        }
-
-        for (let i = 0; i < inventory.length; i++) {
-            const coin = document.createElement('img');
-            coin.src = './img/coin.png';
-            inventory_container.appendChild(coin);
-        }
-    }
-
-    getInventory() {
-        return inventory;
-    }
-
-    placeObject() {
-        // Check if there is an object in front of the model
-        if (this.modelHasObjectInFront() !== false) {
-            console.log("Has an object in front.");
             return;
         }
-        // Check if the model has an object in their inventory
-        if (inventory.length == 0) {
-            console.log("No object in inventory to place.");
-            return;
-        } else {
-            let object = inventory.pop();
-            // Set the position of the object in front of the model
-            const modelDirection = new THREE.Vector3();
-            this.model.getWorldDirection(modelDirection);
-            const objectPosition = this.model.position.clone().addScaledVector(modelDirection, -4); // Place the object 4 units in front of the model
-            object.position.copy(objectPosition);
-            object.position.y = 2;
-            this.scene.add(object);
-            console.log("Object Placed.")
-            try {
-                inventory_container.removeChild(inventory_container.childNodes[0]);
-            } catch (e) {
-                return;
-            }
 
-        }
     }
+}
 
 }
 
